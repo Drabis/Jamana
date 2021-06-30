d2846fba9d14872745df01a7bf6ca1e5bb824b84
Drabis <drabis47@gmail.com>
2021-06-30 00:05:00 -0400

    render the post page

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -2,41 +2,61 @@ import React, { useState, useEffect } from "react";
 import Header from "../../header/Header";
 import SideBar from "../../sideBar/SideBar";
 import CardPost from "../../Card";
+import Posts from "../../posts/Posts";
 import "./home.css";
 import API from "../../../utils/API";
-import {Row} from "reactstrap";
-import axios from "axios"
+import { Row } from "reactstrap";
+import axios from "axios";
 
-export default function Home(props) {
+export default function Home() {
+  // const [posts, setPosts] = useState([]);
+  // const [post1, setPost1] = useState([]);
+  // const [post2, setPost2] = useState([]);
   const [posts, setPosts] = useState([]);
-  const [post1, setPost1] = useState([]);
-  const [post2, setPost2] = useState([]);
-
   useEffect(() => {
-    API.getPostsByUser(props.user).then((response) => {
-      console.log(response);
-      setPosts(response.data[0].posts);
-    });
-    console.log(props.user);
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      setPosts(res.data);
+    };
+    fetchPosts();
   }, []);
 
-  useEffect(() => {
-    handlepostlimit();
-  }, [posts]);
+  return (
+    <>
+     <Header />
+     <div className="home">
+       <Posts posts={posts}/>
+       <SideBar />
+     </div>
+    </>
+  );
+}
 
-  const handlepostlimit = () => {
-    const firstPosts = [];
-    const secondPosts = [];
-    posts.forEach((post, index) => {
-      if (index <= 3) {
-        firstPosts.push(post);
-      } else if (index > 3 && index <= 7) {
-        secondPosts.push(post);
-      }
-      setPost1(firstPosts);
-      setPost2(secondPosts);
-    });
-  };
+  // useEffect(() => {
+  //   API.getPostsByUser(props.user).then((response) => {
+  //     console.log(response);
+  //     setPosts(response.data[0].posts);
+  //   });
+  //   console.log(props.user);
+  // }, []);
+
+  // useEffect(() => {
+  //   handlepostlimit();
+  // }, [posts]);
+
+  // const handlepostlimit = () => {
+  //   const firstPosts = [];
+  //   const secondPosts = [];
+  //   posts.forEach((post, index) => {
+  //     if (index <= 3) {
+  //       firstPosts.push(post);
+  //     } else if (index > 3 && index <= 7) {
+  //       secondPosts.push(post);
+  //     }
+  //     setPost1(firstPosts);
+  //     setPost2(secondPosts);
+  //   });
+  // };
   // const mapPosts = () => {
   //   posts.map((post) => {
   //     return (
@@ -49,36 +69,35 @@ export default function Home(props) {
   //   });
   // };
 
-  return (
-    <>
-      <Header />
-      <div className="home">
-        {/* {mapPosts()} */}
-        <Row>
-          {post1.map((post) => (
-            <CardPost
-              key={post._id}
-              image={post.img}
-              description={post.description}
-              title={post.title}
-              body={post.body.blocks[0].text}
-            />
-          ))}
-        </Row>
-        <Row>
-          {post2.map((post) => (
-            <CardPost
-              key={post._id}
-              image={post.img}
-              description={post.description}
-              title={post.title}
-              body={post.body.blocks[0].text}
-            />
-          ))}
-        </Row>
-
-      </div>
-      <SideBar />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Header />
+//       <div className="home">
+//         {/* {mapPosts()} */}
+//         <Row>
+//           {/* {post1.map((post) => (
+//             <CardPost
+//               key={post._id}
+//               image={post.img}
+//               description={post.description}
+//               title={post.title}
+//               body={post.body.blocks[0].text}
+//             />
+//           ))} */}
+//         </Row>
+//         <Row>
+//           {/* {post2.map((post) => (
+//             <CardPost
+//               key={post._id}
+//               image={post.img}
+//               description={post.description}
+//               title={post.title}
+//               body={post.body.blocks[0].text}
+//             />
+//           ))} */}
+//         </Row>
+//       </div>
+//       <SideBar />
+//     </>
+//   );
+// }
