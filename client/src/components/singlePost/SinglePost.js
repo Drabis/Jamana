af3ed8676f329372878adedc82cccb8ec9bdb628
Drabis <drabis47@gmail.com>
2021-06-30 00:44:31 -0400

    create the single post component

---
+++ client/src/components/singlePost/SinglePost.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function SinglePost() {
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({})
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data)
+    };
+    getPost();
+  }, [path]);
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        {post.photo && (
+          <img className="singlePostImg" src={post.photo} alt="" />
+        )}
+        <h1 className="singlePostTitle">
+          {post.title}
+          <div>
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+            <span className="Author">
+            Author: <b>{post.username}</b>
+            </span>
+            <span className="singlePostDate">{new Date(post.createdAt).toDateString}</span>
+        </div>
+        <p className="singlePostDesc">{post.desc}</p>
+      </div>
+    </div>
+  );
+}
