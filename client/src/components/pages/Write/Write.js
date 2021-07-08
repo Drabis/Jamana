d2d8c431476016fb77efa6fdaea10f2c022c8e04
Drabis <drabis47@gmail.com>
2021-07-08 09:21:16 -0400

    added style to write file

--- client/src/components/pages/Write/Write.js
+++ client/src/components/pages/Write/Write.js
@@ -3,43 +3,43 @@ import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import RichEditor from "../../RichEditor";
 import API from "../../../utils/API";
+import SideBar from "../../sideBar/SideBar";
 import "./style.css";
-
+import { DropdownItem, DropdownToggle, ButtonDropdown } from "reactstrap";
 
 export default function Write(props) {
-  let {postId} = useParams()
+  let { postId } = useParams();
   const history = useHistory();
   const [blog, setBlog] = useState({});
   const [blogInfo, setBlogInfo] = useState({});
   const [parse, setParse] = useState({});
 
   useEffect(() => {
-    if(postId) {
-      API.getPostById(postId)
-      .then(response => {
-        console.log(response)
-        const parsePost = JSON.parse(response.data.body)
+    if (postId) {
+      API.getPostById(postId).then((response) => {
+        console.log(response);
+        const parsePost = JSON.parse(response.data.body);
         setParse(parsePost);
-        setBlogInfo({ title: response.data.title, author: response.data.author})
-      })
+        setBlogInfo({
+          title: response.data.title,
+          author: response.data.author,
+        });
+      });
+    } else {
+      setParse({});
+      setBlogInfo({});
     }
-    else {
-      setParse({})
-      setBlogInfo({})
-    }
-  },[postId])
+  }, [postId]);
 
   const handleInputChange = (e) => {
     setBlogInfo({ ...blogInfo, [e.target.name]: e.target.value });
   };
   const handlePostSubmit = () => {
     if (postId) {
-      API.updateBlog(blog, blogInfo, postId)
-      .then(() => {
-        history.push("/")
-      })
-    }
-    else {
+      API.updateBlog(blog, blogInfo, postId).then(() => {
+        history.push("/");
+      });
+    } else {
       API.submitBlog(blog, blogInfo, props.user).then(() => {
         history.push("/");
       });
@@ -53,15 +53,32 @@ export default function Write(props) {
   };
 
   return (
-    <div>
-      <h2>TITLE</h2>
-      <input type="text" onChange={handleInputChange} value={blogInfo.title || ""} name="title" />
+    <div className="write">
+      <h2>Title</h2>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        value={blogInfo.title || ""}
+        name="title"
+      />
+      <br />
+      <ButtonDropdown>
+        <DropdownToggle caret color="primary">
+          category
+        </DropdownToggle>
+      </ButtonDropdown>
       <h5>Author</h5>
-      <input type="text" onChange={handleInputChange} value={blogInfo.author || ""} name="author" />
+      <input
+        type="text"
+        onChange={handleInputChange}
+        value={blogInfo.author || ""}
+        name="author"
+      />
       <RichEditor blogInfo={blogInfo} post={parse} updateBlog={updateBlog} />
       <button onClick={handlePostSubmit} className="postBtn">
         POST
       </button>
+     
     </div>
   );
 }
