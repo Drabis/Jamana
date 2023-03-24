e3f559bb0ecd253e600001ff0a9bddfccb160e1e
Aboulaye Drabo <drabis47@gmail.com>
2023-03-24 12:59:25 -0400

    fixed register page

--- client/src/components/pages/Register/RegisterPage.js
+++ client/src/components/pages/Register/RegisterPage.js
@@ -1,26 +1,35 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./register.css";
 import { Link, useHistory } from "react-router-dom";
 import API from "../../../utils/API";
 
-export default function SigninPage(props) {
+export default function RegisterPage(props) {
   let history = useHistory();
   const [formInput, setFormInput] = useState({});
-  const [validator, setValidator] = useState({ email: false, name: false });
+  const [validator, setValidator] = useState({ email: false, password: false });
 
   const handleInputChange = (e) => {
     setFormInput({ ...formInput, [e.target.name]: e.target.value });
-    const targetName = e.target.name.value;
+    const targetName = e.target.name;
     switch (targetName) {
       case "email":
         const emailCheck = new RegExp(
           /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
         );
         if (emailCheck.test(e.target.value)) {
-          setFormInput({ ...formInput, [e.target.name]: e.target.value });
           setValidator({ ...validator, email: false });
         } else {
           setValidator({ ...validator, email: true });
+        }
+        break;
+      case "password":
+        const passwordCheck = new RegExp(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        );
+        if (passwordCheck.test(e.target.value)) {
+          setValidator({ ...validator, password: false });
+        } else {
+          setValidator({ ...validator, password: true });
         }
         break;
       default:
@@ -30,19 +39,22 @@ export default function SigninPage(props) {
 
   const handleRegister = (e) => {
     e.preventDefault();
-    API.registerUser(formInput).then((response) => {
-      if (response.status === 200) {
-        props.handleUserSignin();
-        history.push("/");
-      }
-    });
+    API.registerUser(formInput)
+      .then((response) => {
+        if (response.status === 200) {
+          props.handleUserSignin();
+          history.push("/");
+        }
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
-  
     <div className="register">
-
-    <h5 className=" scroll-text"> WELCOME TO JAMANA WHERE THE HISTORY AND THE BEAUTY OF A CONTINENT MEET. READY TO SHARE YOUR EXPERIENCES?</h5> 
+      <h5 className="scroll-text">
+        WELCOME TO JAMANA WHERE THE HISTORY AND THE BEAUTY OF A CONTINENT MEET.
+        READY TO SHARE YOUR EXPERIENCES?
+      </h5>
 
       <span className="registerTitle">Register</span>
       <form className="registerForm">
@@ -68,13 +80,18 @@ export default function SigninPage(props) {
           type="password"
           placeholder="Enter your password"
         />
-        {validator.password && <div>Enter a valid password</div>}
+        {validator.password && (
+          <div className="redText">
+            Password must contain at least 8 characters, one uppercase letter,
+            one lowercase letter, one number, and one special character.
+          </div>
+        )}
 
         <button onClick={handleRegister} className="registerButton">
           register
         </button>
       </form>
-      <button className="registerLoginButon">
+      <button className="registerLoginButton">
         <Link className="link" to="/signin">
           LOGIN
         </Link>
